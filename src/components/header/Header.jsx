7f8e9be99ccf65506c4fd92e459af83d004f9ff8
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-08-03 15:50:28 -0700

    adds basic header text, verified to render on page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useTheme } from '../../hooks/appContext';
 
 const Header = () => {
   return (
     <div>
-      
+      Rick and Morty Characters, the Basics
     </div>
   );
 };
+
+export default Header;
